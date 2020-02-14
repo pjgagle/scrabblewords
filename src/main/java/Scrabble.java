6e7f7483b968a896c766cbfb39c45e9c161137f9
Patrick Gagle <pjgagle@bsu.edu>
2020-02-14 14:20:23 -0500

    updated scrabble.java and added main.java and scrabbletest.java

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,56 +1,65 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Scrabble {
-    private String word;
+    public String word;
+    public int score;
 
-    public Scrabble(String word) {
+    public Scrabble(String word, int score) {
         this.word = word;
+        this.score = score;
     }
 
-    public static int calculateWordValue(String word) {
-
-        int total = 0;
-
-        for (int i = 0; i < word.length(); i++) {
-            total += letterValue(word.charAt(i));
-        }
-
-        return total;
+    public String getWord() {
+        return word;
     }
 
-    public static int letterValue(char letter) {
+    public int getScore() {
+        Map<Character, Integer> letterMap = new HashMap<Character, Integer>();
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-
-
-        if(letter=='A' || letter=='E' || letter=='I' || letter=='O'
-        || letter=='U' || letter=='L' || letter== 'N' || letter=='R'
-        || letter=='S' || letter=='T') {
-            return 1;
+        for (int i = 0; i <letters.length(); i++) {
+            if (letters.charAt(i) == 'A' || letters.charAt(i) == 'E' || letters.charAt(i) == 'I' ||
+                    letters.charAt(i) == 'O' || letters.charAt(i) == 'U' || letters.charAt(i) == 'L' ||
+                    letters.charAt(i) == 'N' || letters.charAt(i) == 'R' || letters.charAt(i) == 'S' ||
+                    letters.charAt(i) == 'T') {
+                letterMap.put(letters.charAt(i), 1);
+                letterMap.put(letters.toLowerCase().charAt(i), 1);
+            }
+            if (letters.charAt(i) == 'D' || letters.charAt(i) == 'G') {
+                letterMap.put(letters.charAt(i), 2);
+                letterMap.put(letters.toLowerCase().charAt(i), 2);
+            }
+            if (letters.charAt(i) == 'B' || letters.charAt(i) == 'C' || letters.charAt(i) == 'M' ||
+                    letters.charAt(i) == 'P') {
+                letterMap.put(letters.charAt(i), 3);
+                letterMap.put(letters.toLowerCase().charAt(i), 3);
+            }
+            if (letters.charAt(i) == 'F' || letters.charAt(i) == 'H' || letters.charAt(i) == 'V' ||
+                    letters.charAt(i) == 'W' || letters.charAt(i) == 'Y') {
+                letterMap.put(letters.charAt(i), 4);
+                letterMap.put(letters.toLowerCase().charAt(i), 4);
+            }
+            if (letters.charAt(i) == 'K') {
+                letterMap.put(letters.charAt(i), 5);
+                letterMap.put(letters.toLowerCase().charAt(i), 5);
+            }
+            if (letters.charAt(i) == 'J' || letters.charAt(i) == 'X') {
+                letterMap.put(letters.charAt(i), 8);
+                letterMap.put(letters.toLowerCase().charAt(i), 8);
+            }
+            if (letters.charAt(i) == 'Q' || letters.charAt(i) == 'Z') {
+                letterMap.put(letters.charAt(i), 10);
+                letterMap.put(letters.toLowerCase().charAt(i), 10);
+            }
         }
+        int totalValue = 0;
 
-        if (letter=='D' || letter=='G') {
-            return 2;
+        for (int j = 0; j < word.length(); j++){
+            totalValue += letterMap.get(word.charAt(j));
         }
-
-        if (letter=='B' || letter=='C' || letter=='M' || letter=='P') {
-            return 3;
-        }
-
-        if (letter=='F' || letter=='H' || letter=='V' || letter=='W'
-        || letter=='Y') {
-            return 4;
-        }
-
-        if (letter=='K') {
-            return 5;
-        }
-
-        if (letter=='J' || letter=='K') {
-            return 8;
-        }
-
-        if (letter=='Q' || letter=='Z') {
-            return 10;
-        }
-
-       return 0;
+        return totalValue;
     }
+
+
 }
